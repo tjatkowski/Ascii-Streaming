21db8f77f562ad00cea5b87a955225846b318dff
Knayder <knayder@outlook.com>
2017-08-20 11:06:00 +0200

    Working version

--- src/Application.cpp
+++ src/Application.cpp
@@ -4,9 +4,9 @@
 
 Application::Application() :
 	windowName("NULL"),
-	window(sf::VideoMode(1280,720), "Ascii Streaming")
+	window(sf::VideoMode(1280, 720), "Ascii Streaming")
 {
-	
+
 }
 
 Application & Application::getInstance()
@@ -17,7 +17,7 @@ Application & Application::getInstance()
 
 
 
-void Application::setWindowName(const std::string newName){
+void Application::setWindowName(const std::string newName) {
 	getInstance().windowName = newName;
 }
 
@@ -58,31 +58,11 @@ void Application::run() {
 		instance.window.clear(sf::Color::Black);
 		instance.window.draw(raw);
 		instance.window.draw(ascii);
-
-		ImageSource raw(sf::IntRect(0, 0, 200, 200), instance.hwnd);
-
-		AsciiDisplay ascii({ 128,72 }, 5);
-
-
-
-		sf::RectangleShape shape;
-		shape.setSize({ 100, 100 });
-
-		std::cout << "Helo\n";
-		while (instance.window.isOpen()) {
-			input();
-
-			raw.update();
-			shape.rotate(1.f);
-			instance.window.clear(sf::Color(37, 37, 48));
-			//instance.window.draw(ascii);
-			instance.window.draw(shape);
-			instance.window.display();
-		}
+		instance.window.display();
 	}
 }
 
-void Application::input(){
+void Application::input() {
 	sf::Event event;
 	Application &instance = getInstance();
 	while (instance.window.pollEvent(event)) {
